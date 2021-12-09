b6dd85a284e1eafc9de2e97b49ec39d6286c58a2
Aryan Singh <79953816+Aryan220402@users.noreply.github.com>
2021-12-09 14:37:41 +0530

    Update RegexExample6.java

--- RegexExample6.java
+++ RegexExample6.java
@@ -4,7 +4,7 @@ public class RegexExample6
     public static void main(String args[])
     {
         System.out.println(Pattern.matches("[a-zA-Z0-9]{6}","2haRAD"));
-        System.out.println(Pattern.matches("[a-zA-Z0-9]{10}","9876543211"));
+        System.out.println(Pattern.matches("[0-9]{10}","9876543211"));
         System.out.println(Pattern.matches("[a-zA-Z0-9]{6}","kkvarun32"));
         System.out.println(Pattern.matches("[a-zA-Z0-9]{6}","JA2UK2"));
         System.out.println(Pattern.matches("[a-zA-Z0-9]{6}","arun$2"));
